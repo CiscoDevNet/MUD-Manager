24c650b7f14c29ca6ba79a5a7d00fa2f8e7813db
Brian Weis <bew@cisco.com>
2018-06-29 10:21:19 -0700

    Add missing files.

---
+++ log.h
@@ -0,0 +1,12 @@
+
+#define MUDC_LOG_INFO(format, args ...) do { \
+    mudc_log("***MUDC [INFO][%s:%d]--> " format "\n", \
+                __func__, __LINE__, ##args); \
+} while (0)
+
+#define MUDC_LOG_ERR(format, args ...) do { \
+    mudc_log("***MUDC [INFO][%s:%d]--> " format "\n", \
+                __func__, __LINE__, ##args); \
+} while (0)
+
+void mudc_log (char *format, ...);
