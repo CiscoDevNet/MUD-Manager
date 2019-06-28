6d74dea8f85b60f2fa4f5310e0cac5743e4d6c0e
Vafa Andalibi <vafa.andalibi@gmail.com>
2019-06-27 22:17:52 -0400

    MUD extension added to openssl-8021ARintermediate.cnf , minor update in run.sh

--- examples/certs_and_keys/run.sh
+++ examples/certs_and_keys/run.sh
@@ -4,10 +4,10 @@
 find . -name "*.sh" | xargs chmod +x
 
 # generating root certificate
-./generate-root-ca.sh
+./generate-ca-root.sh
 
 # generating intermediate certificate
-./generate-intermediate-ca.sh
+./generate-ca-8021AR.sh
 
 # generating iDevID certificate
 ./generate-iDevID-8021AR.sh
